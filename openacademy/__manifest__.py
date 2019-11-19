d8432cdd6a5dc52caa93a57bdfa60225b81600bd
Oscar Nelson <oscar.lanza@agilesolutions.com>
2019-11-19 14:51:16 -0600

    Complete

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -20,14 +20,17 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/openacademy.xml',
         'views/partner.xml',
+        'views/session_board.xml',
+        'reports.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
